ccedef6ce87be75f47e4af5f6080ee739cc0a07e
Patrick Hart <pathart612@gmail.com>
2022-04-07 10:41:09 -0400

    commit tree b933969079f59b630993ebfb0a97c34a9ec9a149

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const greetings = [];
+    for (const person of names){
+        greetings.push(`Thank you, ${person}, for the wonderful surprise gift!`);        
+    }
+    return greetings
+};
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+};
